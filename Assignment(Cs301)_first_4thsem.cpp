f7052900bb1b55e90889599efd3d16a1739a09ad
Laiba Razi Khan <95611241+LaibaRazi@users.noreply.github.com>
2023-05-16 11:08:45 +0000

    incomplete commit

--- Assignment(Cs301)_first_4thsem.cpp
+++ Assignment(Cs301)_first_4thsem.cpp
@@ -1,16 +1,147 @@
 #include <iostream>
-#include<stdlib.h>
-
+#include <stdlib.h>
+using namespace std;
 class Node
 {
 private:
-int object;
-Node* nextNode;
+    int object;
+    Node *nextNode;
+
 public:
-void set(int value)
+    void set(int value)
+    {
+        object = value;
+    }
+    int get() { return object; }
+
+    void setNext(Node *address)
+    {
+        nextNode = address;
+    }
+    Node *getNext()
+    {
+        return nextNode;
+    }
+};
+
+class list
 {
-    object;
-}
+private:
+    Node *Head;
+    Node *Odd;
+    Node *Even;
+    Node *Tail;
+    Node *Odd_Tail;
+    Node *Even_Tail;
+
+public:
+    list()
+    {
+        Head = NULL;
+        Odd = NULL;
+        Even = NULL;
+        Tail = NULL;
+        Odd_Tail = NULL;
+        Even_Tail = NULL;
+    }
+    void insert()
+    {
+        int value;
+        cout << "\nEnter Numeric Character of Your Vu ID :";
+        cin >> value;
+        Node *newNode = new Node;
+        newNode->set(value);
+        newNode->setNext(NULL);
+        if (Head == NULL)
+        {
+            Head = newNode;
+            Tail = newNode;
+        }
+        else
+        {
+            Tail->setNext(newNode);
+            Tail = newNode;
+        }
+    }
+    void Odd_List()
+    {
+        Node *ptr = Head;
+        while (ptr != NULL)
+        {
+            if (ptr->get() % 2 != 0)
+            {
+                Node *newNode = new Node;
+                newNode->set(ptr->get());
+                newNode->setNext(NULL);
+                if (Odd == NULL)
+                {
+                    Odd = newNode;
+                    Odd_Tail = newNode;
+                }
+                else
+                {
+                    Odd_Tail->setNext(newNode);
+                    Odd_Tail = newNode;
+                }
+            }
+            ptr = ptr->getNext();
+        }
+        ptr = Odd;
+        while (ptr != NULL)
+        {
+            cout << "List Element " << ptr->get() << "\n";
+            ptr = ptr->getNext();
+        }
+    }
+
+    void Even_list()
+    {
+        Node *ptr = Head;
+        while (ptr != NULL)
+        {
+            if (ptr->get() % 2 == 0)
+            {
+                Node *newNode = new Node;
+                newNode->set(ptr->get());
+                newNode->setNext(NULL);
+                if (Even = NULL)
+                {
+                    Even = newNode;
+                    Even_Tail = newNode;
+                }
+                else
+                {
+                    Even_Tail->setNext(newNode);
+                    Even_Tail = newNode;
+                }
+            }
+            ptr = ptr->getNext();
+        }
+        ptr = Even;
+        while (ptr != NULL)
+        {
+            cout << "List Element" << ptr->get() << "\n";
+            ptr = ptr->getNext();
+        }
+    }
+
+    void Merge_List()
+    {
+        Head=NULL;
+        Node* ptr = Odd;
+        while(ptr != NULL)
+        {
+            Node* newNode =new Node;
+            newNode -> set(ptr -> get());
+            newNode -> setNext(NULL);
+            if(Head == NULL)
+            {
+                Node* newNode = new Node;
+                newNode -> set(ptr -> get());
+            }
+
+        }
+    }
 };
 int main()
 {
